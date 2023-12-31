b84e15e024017bf97e2cc0b463a4d3903f743ebd
Kashish Mistry <kashish.mistry3@gmail.com>
2023-12-31 13:34:50 -0500

    updated media queries for screen size compatibility

--- src/App.js
+++ src/App.js
@@ -15,28 +15,28 @@ function Header() {
                 <div className='contact'>
                   <div className='bubble'>
                       <a href='mailto: kashish.mistry@mail.utoronto.ca'>
-                        <img style={{ width:40, height:40}} src="email.png" alt='Email icon'></img>
+                        <img className="contact-logo" src="email.png" alt='Email icon'></img>
                       </a>
                   </div>
                   <div className="bubble">
                       <a href='tel: 647-629-6876'>
-                        <img style={{ width:40, height:40}} src="phone.png" alt='Phone icon'></img> 
+                        <img className="contact-logo" src="phone.png" alt='Phone icon'></img> 
                       </a>
                   </div>
                   <div className='bubble'>
                     <a href="https://www.linkedin.com/in/kashish-mistry/">
-                      <img style={{ width:40, height:40}} src="linkedin.png" alt='Github logo'></img>
+                      <img className="contact-logo" src="linkedin.png" alt='Github logo'></img>
                     </a>
                   </div>
                   <div className='bubble'>
                     <a href="https://github.com/KashishMistry">
-                    <img style={{ width:40, height:40}} src="githublogo.png" alt='Github logo'></img>
+                    <img className="contact-logo" src="githublogo.png" alt='Github logo'></img>
                     </a>
                   </div>
                 </div>
             </div>
             <div>
-                <img src="profile_pic.png" alt="Headshot of Kashish Mistry" />
+                <img className="profile-img" src="profile_pic.png" alt="Headshot of Kashish Mistry" />
             </div>
         </div>
         
@@ -51,8 +51,8 @@ function Education() {
       <div className="wrap-content">
           <h2>Education.</h2>
           <h3>Bachelor of Applied Science in Computer Engineering, Minor in Artificial Intelligence Engineering <br></br> <i>University of Toronto</i></h3>
-          <div className="bubble">
-              <h4>Completed Relevant Coursework:</h4>
+          <div className="bubble education">
+              <h4>Completed Relevant Coursework</h4>
               <p className="description">Applied Fundamentals of Deep Learning, Computer Networks I, 
                   Introduction to Databases, Engineering Economic Analysis & Entrepreneurship, 
                   Operating Systems, Algorithms & Data Structures,
@@ -61,8 +61,8 @@ function Education() {
                   Engineering Strategies & Practice
               </p>
           </div>
-          <div className="bubble">
-              <h4>Achievements:</h4>
+          <div className="bubble education">
+              <h4>Achievements</h4>
               <p class="description">
                 Ontario Professional Engineers Foundation for Education Undergraduate Scholarship, Colantonio Family Leadership Award, Dean's Honours List (Winter 2023, Fall 2020)
               </p>
@@ -78,28 +78,32 @@ class Work extends React.Component {
 		this.state = { data: [] }
 	}
 
-  work_exp = [{"title": "Software Developer Intern - Environment and Climate Change Canada", 
+  work_exp = [{"title": "Software Developer Intern",
+                "company": "Environment and Climate Change Canada", 
                 "date": "September 2023 - Present",
                 "description_one":"Developing a reusable and accessible open-source mapping platform using Vue.js, TypeScript, and ESRI Mapping API to present environmental data using interactive maps",
                 "description_two":"Developing a comprehensive inventory management tool from inception to full deployment using Microsoft Power Apps, optimizing inventory tracking and management processes for enhanced efficiency",
                 "description_three":"Leveraging Git for streamlined code collaboration and version control within cross-functional teams",
                 "logo": "eccc.png",
                 "alt":"Environment and Climate Change logo"},
-              {"title": "Research Student - Youth Climate Lab", 
+              {"title": "Research Student",
+                "company": "Youth Climate Lab", 
                 "date": "August 2023 - Present",
                 "description_one":"Conducting an independent research project to investigate the implications of Artificial Intelligence (AI) on climate change, with a focus on ethical considerations, algorithm fairness, and carbon cost",
                 "description_two":"Will be publishing article on the ethical considerations of climate change artificial intelligence in Starfish Journal",
                 "description_three":"Developing an \"Earth and AI\" handbook to provide insights to the general public about the how AI will impact climate change",
                 "logo": "ycl.png",
                 "alt":"Youth Climate Lab logo"},
-              {"title": "Developer - RBC Amplify", 
+              {"title": "Developer", 
+                "company": "RBC Amplify",
                 "date": "May 2023 - August 2023",
                 "description_one":"Building a system using large-language models to automate data extraction from client documents to improve processes across all lines of business at RBC",
                 "description_two":"Developing a progressive user interface using React.js and Flask to improve employee experience",
                 "description_three":"Managing project workflow using Agile methodologies (Kanban) and Confluence to meet deadlines and objectives",
                 "logo": "rbc.png",
                 "alt":"Royal Bank of Canada logo"}, 
-              {"title":"Research Assistant - University of Toronto",
+              {"title":"Research Assistant",
+                "company": "University of Toronto",
                 "date":"May 2022 - August 2022",
                 "description_one":"Awarded \"Best Podium Presentation – Transdisciplinary Engineering Education & Practice\" at the University of Toronto Undergraduate Engineering Research Day (UnERD) 2022",
                 "description_two":"Research Paper will soon be published by American Society for Engineering Education (ASEE) and Abstract soon be to published in STEM Fellowship Journal",
@@ -114,7 +118,7 @@ class Work extends React.Component {
         { this.work_exp.map((item, i) => {
           return <div className="bubble">
               <div>
-                <h3>{item.title}</h3>
+                <h3>{item.title} <br></br> <i>{item.company}</i></h3>
                 <p class="date">{item.date}</p>
                 <ul class="description">
                   <li>{item.description_one}</li>
@@ -139,35 +143,40 @@ class Extracurricular extends React.Component {
 		this.state = { data: [] }
 	}
 
-  extra_exp = [{"title":"Stories of WISE Editor - Women in Science and Engineering UofT", 
+  extra_exp = [{"title":"Stories of WISE Editor", 
+                "company": "Women in Science and Engineering UofT",
                 "date":"August 2023 - Present",
                 "description_one":"Curate and edit engaging content that highlights achievements, innovations, and challenges faced by women in science and engineering",
                 "description_two":"Foster a diverse and inclusive narrative by ensuring balanced representation of voices and perspectives within the magazine",
                 "logo": "wise.png",
                 "alt":"Women in Science and Engineering, University of Toronto logo",
                 "class":"wise"},
-              {"title":"Software Developer - UTWind", 
+              {"title":"Software Developer - UTWind",
+                "company": "UTWind", 
                 "date":"Sept 2022 - April 2023",
                 "description_one":"Debugged Fast API implementation associated with mobile app to receive data from wind turbine, resulting in data collection stability",
                 "description_two":"Employed critical thinking techniques to isolate and repair software design defects in both front and back-end source codes, improving application stability",
                 "logo":"utwind.png",
                 "alt":"UTWind logo",
                 "class":"utwind"}, 
-              {"title":"Conference Workshops Director - Women in Science and Engineering UofT",
+              {"title":"Conference Workshops Director",
+                "company": "Women in Science and Engineering UofT",
                 "date":"August 2022 - April 2023",
                 "description_one":"Organized 5 workshops for the Women in Science and Engineering National Conference (WISENC) by working with sponsor companies to cater to the interest of over 300 participants",
                 "description_two":"Supporting the 50-person executive team in leading a full-day event with over 20 sponsor companies",
                 "logo": "wise.png",
                 "alt":"Women in Science and Engineering, University of Toronto logo",
                 "class":"wise"}, 
-              {"title":"Sustainability Researcher - UTWind",
+              {"title":"Sustainability Researcher",
+                "company": "UTWind",
                 "date":"June 2021 - August 2022",
                 "description_one":"Contributed to 1st place win at the International Small Wind Turbine Competition (ISTWC 2022) through sustainability research",
                 "description_two":"Analyzed local challenges, such as lack of access to charging stations and water purifiers, in the Kesses Region in Kenya, to evaluate the most effective use for the electricity generated by the future wind turbine",
                 "logo": "utwind.png",
                 "alt":"UTWind logo",
                 "class":"utwind"}, 
-              {"title":"Ombudsperson - University of Toronto Engineering Society",
+              {"title":"Ombudsperson",
+                "company": "University of Toronto Engineering Society",
                 "date":"June 2021 - June 2022",
                 "description_one":"Designed a census for over 5000 engineering undergraduate student to compile a report of trends to improve services for students",
                 "description_two":"Mediated internal complaints and recommending outcomes taking opinions of both parties into account",
@@ -182,7 +191,7 @@ class Extracurricular extends React.Component {
         { this.extra_exp.map((item, i) => {
           return <div className="bubble">
               <div>
-                <h3>{item.title}</h3>
+                <h3>{item.title} <br></br> <i>{item.company}</i></h3>
                 <p className="date">{item.date}</p>
                 <ul className="description">
                   <li>{item.description_one}</li>
